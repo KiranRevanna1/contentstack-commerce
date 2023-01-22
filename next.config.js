94bc1b2d0d173fda139c1119bd87cf965e5df1ef
KiranRevanna1 <87458505+KiranRevanna1@users.noreply.github.com>
2023-01-22 02:53:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  i18n: {
+    locales: ['en-US', 'es'],
+    defaultLocale: 'en-US',
+  },
+  images: {
+    domains: ['images.contentstack.io'],
+  },
+}
